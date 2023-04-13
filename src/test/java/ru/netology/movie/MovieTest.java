3c418df8d31934bfba1a5e5d95f6ca39a54b740b
Boldyrev Anatoly <boldirev87@mail.ru>
2023-04-13 19:38:21 +0400

    added new test

--- src/test/java/ru/netology/movie/MovieTest.java
+++ src/test/java/ru/netology/movie/MovieTest.java
@@ -42,6 +42,42 @@ public class MovieTest {
 
     @Test
     public void test3() {
+        ManagerMovie manager = new ManagerMovie(6);
+
+        manager.addMovie("Dracula 1992");
+        manager.addMovie("Speed 1994");
+        manager.addMovie("The Devil's Advocate 1997");
+        manager.addMovie("Matrix 1999");
+        manager.addMovie("Constantine 2005");
+        manager.addMovie("John Wick 2014");
+        manager.addMovie("Ballerina 2024");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Ballerina 2024", "John Wick 2014", "Constantine 2005", "Matrix 1999", "The Devil's Advocate 1997", "Speed 1994"};
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void test4() {
+        ManagerMovie manager = new ManagerMovie(3);
+
+        manager.addMovie("Dracula 1992");
+        manager.addMovie("Speed 1994");
+        manager.addMovie("The Devil's Advocate 1997");
+        manager.addMovie("Matrix 1999");
+        manager.addMovie("Constantine 2005");
+        manager.addMovie("John Wick 2014");
+        manager.addMovie("Ballerina 2024");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Ballerina 2024", "John Wick 2014", "Constantine 2005"};
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void test5() {
         ManagerMovie manager = new ManagerMovie();
 
         manager.addMovie("Dracula 1992");
